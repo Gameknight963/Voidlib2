358e928de0e1b19256b18f822659d7536bcf4ca4
Gameknight963 <sebisebpaw@gmail.com>
2026-04-19 19:01:59 -0500

    switch from image to sprite

--- Inventory/ItemDefinition.cs
+++ Inventory/ItemDefinition.cs
@@ -1,13 +1,14 @@
 ﻿using UnityEngine.UI;
+using UnityEngine;
 
 namespace VoidLib2.Inventory
 {
     public class ItemDefinition
     {
         public string Id { get; private set; }
-        public Image? Image { get; private set; }
+        public Sprite? Image { get; private set; }
         public string Name { get; private set; }
-        public ItemDefinition(string id, string name, Image? image = null)
+        public ItemDefinition(string id, string name, Sprite? image = null)
         {
             Id = id;
             Name = name;
