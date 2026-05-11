c9a5f61375ca08db9ccc7388a083b3c33a4b10cc
Gameknight963 <sebisebpaw@gmail.com>
2026-05-10 22:13:01 -0500

    add audioimporter

---
+++ AudioImporter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace VoidLib2
+{
+    public static class AudioImporter
+    {
+        public static AudioClip? LoadAudio(string filePath)
+        {
+            // Flags: BASS_SAMPLE_FLOAT (256) | BASS_STREAM_DECODE (2097152)
+            uint flags = 256 | 2097152;
+
+            NativeBass.BASS_Init(-1, 44100, 0, IntPtr.Zero, IntPtr.Zero);
+
+            int handle = NativeBass.BASS_StreamCreateFile(false, filePath, 0, 0, flags);
+            if (handle == 0) return null;
+            NativeBass.BASS_ChannelGetInfo(handle, out NativeBass.BASS_CHANNELINFO info);
+            long lengthBytes = NativeBass.BASS_ChannelGetLength(handle, 0); // BASS_POS_BYTE
+            int totalSamples = (int)(lengthBytes / 4);
+            float[] sampleBuffer = new float[totalSamples];
+            NativeBass.BASS_ChannelGetData(handle, sampleBuffer, (int)lengthBytes);
+            AudioClip clip = AudioClip.Create(
+                Path.GetFileName(filePath),
+                totalSamples / info.chans,
+                info.chans,
+                info.freq,
+                false
+            );
+            clip.SetData(sampleBuffer, 0);
+            NativeBass.BASS_StreamFree(handle);
+            return clip;
+        }
+    }
+}
---
+++ NativeBass.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.InteropServices;
+
+namespace VoidLib2
+{
+    // get base.dll at https://www.un4seen.com/
+
+    public class NativeBass
+    {
+        private const string BASS_LIB = "bass";
+
+        [DllImport(BASS_LIB)]
+        public static extern bool BASS_Init(int device, int freq, uint flags, IntPtr win, IntPtr clsid);
+
+        [DllImport(BASS_LIB)]
+        public static extern int BASS_StreamCreateFile(bool mem, string file, long offset, long length, uint flags);
+
+        [DllImport(BASS_LIB)]
+        public static extern bool BASS_ChannelGetInfo(int handle, out BASS_CHANNELINFO info);
+
+        [DllImport(BASS_LIB)]
+        public static extern long BASS_ChannelGetLength(int handle, uint mode);
+
+        [DllImport(BASS_LIB)]
+        public static extern int BASS_ChannelGetData(int handle, [Out] float[] buffer, int length);
+
+        [DllImport(BASS_LIB)]
+        public static extern bool BASS_StreamFree(int handle);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct BASS_CHANNELINFO
+        {
+            public int freq;
+            public int chans;
+            public uint flags;
+            public uint ctype;
+            public uint origres;
+            public int plugin;
+            public int sample;
+            public IntPtr filename;
+        }
+    }
+}
