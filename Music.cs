f81d6e29f61c59a5f6953d577dc6fc13e12408db
Gameknight963 <sebisebpaw@gmail.com>
2026-05-10 23:31:18 -0500

    add helper to mute music without touching settings

---
+++ Music.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace VoidLib2
+{
+    public static class Music
+    {
+        public static bool SetMusicMuted(bool muted)
+        {
+            GameObject obj = GameObject.Find("World/Ambience/Ambient Music");
+            if (obj == null) return false;
+            obj.GetComponent<AudioSource>().mute = muted;
+            return true;
+        }
+    }
+}
