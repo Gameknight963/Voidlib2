6618db07f268ebee424413753eb2b2e168d1b7c0
Gameknight963 <sebisebpaw@gmail.com>
2026-05-11 00:00:42 -0500

    fix: find settings using findobjectoftype cause the singleton instance is never assigned for some reason

--- Settings.cs
+++ Settings.cs
@@ -1,12 +1,17 @@
 ﻿using Il2Cpp;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
+using static MelonLoader.MelonLogger;
 
 namespace VoidLib2
 {
     public static class Settings
     {
         private static readonly System.Reflection.FieldInfo? _colorGradingField;
+
+        private static SettingsManager? _instance;
+        private static SettingsManager Instance =>
+            _instance ??= GameObject.FindObjectOfType<SettingsManager>();
 
         static Settings()
         {
@@ -20,65 +25,65 @@ namespace VoidLib2
         /// <summary>Master volume in the range (0, 1].</summary>
         public static float MasterVolume
         {
-            get => SettingsManager.Instance.settings.masterVolume;
+            get => Instance.settings.masterVolume;
             set
             {
-                SettingsManager.Instance.settings.masterVolume = value;
-                SettingsManager.Instance.mixer.SetFloat("Master", Mathf.Log10(value) * 20f);
+                Instance.settings.masterVolume = value;
+                Instance.mixer.SetFloat("Master", Mathf.Log10(value) * 20f);
             }
         }
 
         /// <summary>Music volume in the range (0, 1].</summary>
         public static float MusicVolume
         {
-            get => SettingsManager.Instance.settings.musicVolume;
+            get => Instance.settings.musicVolume;
             set
             {
-                SettingsManager.Instance.settings.musicVolume = value;
-                SettingsManager.Instance.mixer.SetFloat("Music", Mathf.Log10(value) * 20f);
+                Instance.settings.musicVolume = value;
+                Instance.mixer.SetFloat("Music", Mathf.Log10(value) * 20f);
             }
         }
 
         /// <summary>SFX volume in the range (0, 1].</summary>
         public static float SFXVolume
         {
-            get => SettingsManager.Instance.settings.fxVolume;
+            get => Instance.settings.fxVolume;
             set
             {
-                SettingsManager.Instance.settings.fxVolume = value;
-                SettingsManager.Instance.mixer.SetFloat("SFX", Mathf.Log10(value) * 20f);
+                Instance.settings.fxVolume = value;
+                Instance.mixer.SetFloat("SFX", Mathf.Log10(value) * 20f);
             }
         }
 
         /// <summary>Vocal volume in the range (0, 1].</summary>
         public static float VocalVolume
         {
-            get => SettingsManager.Instance.settings.vocalVolume;
+            get => Instance.settings.vocalVolume;
             set
             {
-                SettingsManager.Instance.settings.vocalVolume = value;
-                SettingsManager.Instance.mixer.SetFloat("Vocals", Mathf.Log10(value) * 20f);
+                Instance.settings.vocalVolume = value;
+                Instance.mixer.SetFloat("Vocals", Mathf.Log10(value) * 20f);
             }
         }
 
         /// <summary>Ambience volume in the range (0, 1].</summary>
         public static float AmbienceVolume
         {
-            get => SettingsManager.Instance.settings.ambient;
+            get => Instance.settings.ambient;
             set
             {
-                SettingsManager.Instance.settings.ambient = value;
-                SettingsManager.Instance.mixer.SetFloat("Ambience", Mathf.Log10(value) * 20f);
+                Instance.settings.ambient = value;
+                Instance.mixer.SetFloat("Ambience", Mathf.Log10(value) * 20f);
             }
         }
 
         /// <summary>Whether the game runs in fullscreen mode.</summary>
         public static bool Fullscreen
         {
-            get => SettingsManager.Instance.settings.fullscreen;
+            get => Instance.settings.fullscreen;
             set
             {
-                SettingsManager.Instance.settings.fullscreen = value;
+                Instance.settings.fullscreen = value;
                 Screen.fullScreen = value;
             }
         }
@@ -86,10 +91,10 @@ namespace VoidLib2
         /// <summary>Whether VSync is enabled.</summary>
         public static bool VSync
         {
-            get => SettingsManager.Instance.settings.VSync;
+            get => Instance.settings.VSync;
             set
             {
-                SettingsManager.Instance.settings.VSync = value;
+                Instance.settings.VSync = value;
                 QualitySettings.vSyncCount = value ? 1 : 0;
             }
         }
@@ -97,11 +102,11 @@ namespace VoidLib2
         /// <summary>Field of view in degrees. Also applies to the player camera if available.</summary>
         public static float FOV
         {
-            get => SettingsManager.Instance.settings.fov;
+            get => Instance.settings.fov;
             set
             {
-                SettingsManager.Instance.settings.fov = value;
-                SettingsManager.Instance.fov = value;
+                Instance.settings.fov = value;
+                Instance.fov = value;
                 if (PlayerManager.instance && PlayerManager.instance.playerCam)
                     PlayerManager.instance.playerCam.fieldOfView = value;
             }
@@ -110,11 +115,11 @@ namespace VoidLib2
         /// <summary>Mouse sensitivity. Also applies to the player look component if available.</summary>
         public static float Sensitivity
         {
-            get => SettingsManager.Instance.settings.sensitivity;
+            get => Instance.settings.sensitivity;
             set
             {
-                SettingsManager.Instance.settings.sensitivity = value;
-                SettingsManager.Instance.sensitivity = value;
+                Instance.settings.sensitivity = value;
+                Instance.sensitivity = value;
                 if (PlayerManager.instance && PlayerManager.instance.look)
                     PlayerManager.instance.look.sensitivity = value;
             }
@@ -123,10 +128,10 @@ namespace VoidLib2
         /// <summary>Screen brightness via post-processing color grading.</summary>
         public static float Brightness
         {
-            get => SettingsManager.Instance.settings.brightness;
+            get => Instance.settings.brightness;
             set
             {
-                SettingsManager.Instance.settings.brightness = value;
+                Instance.settings.brightness = value;
                 ColorGrading? cg = GetColorGrading();
                 if (cg != null)
                     cg.brightness.value = value;
@@ -136,10 +141,10 @@ namespace VoidLib2
         /// <summary>Quality level index as defined in Unity's Quality Settings.</summary>
         public static int Quality
         {
-            get => (int)SettingsManager.Instance.settings.qualityIndex;
+            get => (int)Instance.settings.qualityIndex;
             set
             {
-                SettingsManager.Instance.settings.qualityIndex = value;
+                Instance.settings.qualityIndex = value;
                 QualitySettings.SetQualityLevel(value);
             }
         }
@@ -147,12 +152,12 @@ namespace VoidLib2
         /// <summary>Anti-aliasing level as a 0-3 index into { 0, 2, 4, 8 } sample counts.</summary>
         public static int AntiAliasing
         {
-            get => Mathf.RoundToInt(SettingsManager.Instance.settings.aaLevel);
+            get => Mathf.RoundToInt(Instance.settings.aaLevel);
             set
             {
                 int[] aaOptions = { 0, 2, 4, 8 };
                 value = Mathf.Clamp(value, 0, aaOptions.Length - 1);
-                SettingsManager.Instance.settings.aaLevel = value;
+                Instance.settings.aaLevel = value;
                 QualitySettings.antiAliasing = aaOptions[value];
             }
         }
